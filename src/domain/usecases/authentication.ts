a3dbc5145787252b9d5484abac6f445d275e40fc
Vinicius Nogueira <viniciusriemma@gmail.com>
2020-06-21 04:05:51 -0300

    refactor: change Authentication to receive a model

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,3 +1,8 @@
+export interface AuthenticationModel {
+  email: string
+  password: TemplateStringsArray
+}
+
 export interface Authentication {
-  auth (email: string, password: string): Promise<string>
+  auth (authentication: AuthenticationModel): Promise<string>
 }
