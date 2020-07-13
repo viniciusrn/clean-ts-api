09b10c8bc0e244daf5f146370f3b82fd4c1f1412
Vinicius Nogueira <viniciusriemma@gmail.com>
2020-07-12 21:59:09 -0300

    refactor: change Promises to use shorter version

--- src/data/usecases/account/authentication/db-authentication.spec.ts
+++ src/data/usecases/account/authentication/db-authentication.spec.ts
@@ -74,7 +74,7 @@ describe('DbAuthtenticase UseCase', () => {
 
   test('Should return null if HashCompare returns false', async () => {
     const { sut, hashComparerStub } = makeSut()
-    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(Promise.resolve(false))
     const accessToken = await sut.auth(mockAuthentication())
     expect(accessToken).toBeNull()
   })
