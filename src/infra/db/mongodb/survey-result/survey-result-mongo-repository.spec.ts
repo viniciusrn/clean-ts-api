dda51b9f7432cb579c5f78259bc8e0bb259ac893
Vinicius Nogueira <viniciusriemma@gmail.com>
2020-07-19 17:14:13 -0300

    test: ensure SurveyResultMongoRepository returns null on load survey without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -135,5 +135,12 @@ describe('Survey Mongo Repository', () => {
       expect(surveyResult.answers[2].count).toBe(0)
       expect(surveyResult.answers[2].percent).toBe(0)
     })
+
+    test('Should return null on if there is no surveyresult', async () => {
+      const survey = await makeSurvey()
+      const sut = makeSut()
+      const surveyResult = await sut.loadBySurveyId(survey.id)
+      expect(surveyResult).toBeNull()
+    })
   })
 })
